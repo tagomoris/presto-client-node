e3a48f007495b71cbf89b4b8ec0fb14e625192f2
John Groves <johngroves310@gmail.com>
2017-01-16 22:19:33 -0800

    Add tests and mock Presto coordinator server

---
+++ test/index.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+var mock_data = require('./mocks/mock-data');
+var server = require('./mocks/mock-coordinator');
+
+var presto = require('../index');
+var client = new presto.Client({user: 'myname', catalog: 'hive', schema: 'default'});
+
+describe('Requests', function() {
+    describe('GET Nodes', function() {
+        it('Should return presto cluster list.', function(done) {
+            client.nodes({}, function(error,data) {
+                assert.deepEqual(data,mock_data.getNode,'Get nodes did not return expected data.');
+                done()
+            })
+        });
+    });
+
+    describe('GET Query', function() {
+        it('Should return query status.', function(done) {
+            client.query('20170115_002017_00002_e5y5i', function(error,data) {
+                assert.deepEqual(data,mock_data.getQuery,'Get Query did not return expected data.');
+                done()
+            })
+        });
+
+        it('should throw error when server does not respond with 200.', function(done) {
+            client.query('20170115_002017_00002_e5y5j', function(error,data) {
+                assert.equal(error.code, 410)
+                assert(error.message.indexOf('query info api returns error') === 0)
+                done()
+            })
+        });
+
+
+    });
+});
---
+++ test/mocks/mock-coordinator.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var data = require('./mock-data');
+var app = express();
+
+
+app.get('/', function (req, res) {
+    res.send('OK')
+});
+
+app.get('/v1/node', function (req, res) {
+    res.send(data.getNode)
+});
+
+app.get('/v1/query/20170115_002017_00002_e5y5i', function (req, res) {
+    res.send(data.getQuery)
+});
+
+app.get('/v1/query/20170115_002017_00002_e5y5j', function (req, res) {
+    res.sendStatus(410)
+});
+
+app.listen(8080, function () {
+    console.log('Mock Presto Coordinator Running on Port 8080.')
+});
